c5064f9eacab93538b74ab7a30966661ab4c8493
Paulo <pc5783346@gmail.com>
2022-07-07 01:30:06 -0300

    feat: the '/movies' route returns the list of movies

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import defaultRouter from "./routes/defaultRouter.js";
 
 dotenv.config();
 
 const server = express();
+
+server.use(cors());
+server.use(defaultRouter);
 
 const PORT = process.env.PORT;
 
